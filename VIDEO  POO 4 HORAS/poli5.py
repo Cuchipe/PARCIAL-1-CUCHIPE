0d52587d7fc988bee4a31f84c67d9da0312fad72
Cuchipe <150275981+Cuchipe@users.noreply.github.com>
2024-01-25 21:32:23 -0500

    Delete VIDEO  POO 4 HORAS directory

--- VIDEO  POO 4 HORAS/poli5.py
+++
@@ -1,9 +0,0 @@
-
-def recorrer(elemento):
-    for item in elemento:
-        print(f"El elemento actual es: {item}")
-        
-    lista =  [1,2,3,4]
-    lista2 = ["maquina","como"]
-
-    recorrer(lista2)
